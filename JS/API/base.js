edab4a05ee19a402a4442cba0c1d71d3898ff52c
Malani Prince <106840579+malani-prince@users.noreply.github.com>
2023-12-15 19:34:21 +0530

    JavaScript concepts
    new concepts added into my List 🏆
    -> Await
    -> Asynchronous
    -> Synchronous
    -> callBack
    -> callBackHall | plant of Doom
    -> inner functionality of Async and await
    
    Date: 15/12/2023

---
+++ JS/API/base.js
@@ -0,0 +1,94 @@
+/**
+ * -----------------------------------------------------------
+ *                          Fetch API
+ * -----------------------------------------------------------
+ */
+
+// Json => JavaScript Object Notation
+/**
+ * javascript can be use for the sending and return the information from  the network, when we needed. (AJAX)
+
+ * fetch is the method that allows this kind of functionality in js 
+
+ * let promise = fetch(
+    <url>,
+    [Option] = "<"get", "post", "delete", etc..>")
+
+ * It Getting the Response in a 2-stage process. 
+
+ * 1. contain two properties ==> a) status   b) ok
+ * 2. After we call the another method to access the body in different formate...  
+ * a) response.text() -> read and return the text
+ * b) response.json() -> 
+ * 
+
+ */
+
+
+// let url = "https://goweather.herokuapp.com/weather/{city}"
+
+// // p is Promise | 
+// let p = fetch("https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME")
+
+// first then is use for the Check the Status whether the "status" is positive and ok === true.  Outcome: 200 , true
+// stage first is made for checking the server is working properly or not.
+
+// second then is use for the display the Content in json or any formate we want 
+// second then return the Object pass by the first then wether it is a Json or Text
+
+// p.then(
+//     (value) => {
+//         console.log("\n" + value.status + "\n")
+//         console.log("\n" + value.ok + "\n")
+//         return value.json()
+//         // return value.text()
+//     }
+// ).then(
+//     (value2) => {
+//         console.log(value2)
+//     }
+// )
+
+// when we have to work with the Fetch api and Asynchronous we use the await keyword
+
+/**
+ * -----------------------------------------------------------
+ *                        POST Request
+ * -----------------------------------------------------------
+ */
+// for making the "post" Request we need to use the "fetch option"
+
+const createToDo = async (todo) => {
+
+    let options = {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(todo),
+    }
+    let p = await fetch('https://jsonplaceholder.typicode.com/todos/', options)
+    let response = await p.json()
+    return response
+}
+// .then(response => response.json())
+// .then(json => console.log(json))   //Outcome: { title: 'foo', body: 'bar', useId: 1, id: 201 }
+
+const getToDo = async (id) => {
+    let response = await fetch('https://jsonplaceholder.typicode.com/todos/' + id)
+    let r = await response.json()
+    return r
+}
+
+const mainFunction = async () => {
+    const todoR = {
+        title: 'harry',
+        body: 'bhai',
+        useId: 1100
+    }
+    let todoReturnObject = await createToDo(todoR)
+    console.log(todoReturnObject)
+    console.log(await getToDo(3))
+}
+
+mainFunction()
--- JS/Async_Await_Promises/base.js
+++ JS/Async_Await_Promises/base.js
@@ -18,12 +18,12 @@
 
 
 /**
- * ----------------------------------------------------- 
+ * -----------------------------------------------------
  *           Synchronous | And | Asynchronous
- * ----------------------------------------------------- 
+ * -----------------------------------------------------
 */
-
-// JS working -> Top down system 
+// Asynchronous is an activity which start at any particular time and end at any time 
+// JS working -> Top down system
 // it is the Synchronous manner
 
 // console.log(" I ")
@@ -35,7 +35,7 @@
 
 // console.log(" Spoon ")
 
-// // Regular function 
+// // Regular function
 // function qwertyuiop(a, b, c) {
 
 // }
@@ -55,11 +55,11 @@
 
 
 /**
- * ----------------------------------------------------- 
+ * -----------------------------------------------------
  *           Call Back
- * ----------------------------------------------------- 
+ * -----------------------------------------------------
 */
-
+// CallBack based of Asynchronous Programming
 // function one() {
 //     console.log("Step - 1")
 // }
@@ -130,7 +130,7 @@
 
 // // This the the main Function calling. [Call back Starts from hear]
 // let order = (fruitName, call_production) => {
-//     // after 2s it starts working 
+//     // after 2s it starts working
 //     setTimeout(() => {
 //         // Back tick
 //         console.log(`${stocks.fruits[fruitName]} wa selected`
@@ -139,14 +139,36 @@
 //     }, 2000);
 // }
 
+// Example of the multiple CallBacks
+
+
+// let fun1 = (callBack1, callBack2) => {
+//     setTimeout(() => {
+//         console.log('Hello Every one this is the Function - 1')
+//     }, 2000)
+
+//     callBack1()
+//     callBack2()
+// }
+
+// let fun2 = () => {
+//     console.log("Hello every One, this is Function - 2")
+// }
+
+// let fun3 = () => {
+//     console.log("Hello every One, this is Function - 3")
+// }
+
+// fun1(fun2, fun3)
 
 
 /**
- * ----------------------------------------------------- 
- *                   CallBack Hell Created
- * ----------------------------------------------------- 
+ * -----------------------------------------------------
+ *        CallBack Hell Created | Pyramid of Doom
+ * -----------------------------------------------------
 */
-
+// code Readability is destroyed the code readability 
+// Code Grew left side direction 
 // let production = () => {
 //     setTimeout(() => {
 
@@ -155,7 +177,7 @@
 //         console.log("Production has Started....[2s]")
 
 //         /**  ----------- Chop the Fruit ----------- */
-//         // now for the further more process we add again add set Time out function 
+//         // now for the further more process we add again add set Time out function
 //         setTimeout(() => {
 //             console.log("the fruit has been Chopped....[2s]")
 
@@ -201,9 +223,11 @@
 // To avoid the callBack Hell we come up with the solution, Called Promises
 
 
+
+
 /**
  * -----------------------------------------------------
- *                      Promise 
+ *                      Promise
  * -----------------------------------------------------
 */
 
@@ -231,56 +255,396 @@
 // 3. Error Handling
 // 4. The finally Block
 
-let stocks = {
-    fruits: [
-        "strawberry",
-        "grapes",
-        "Apple",
-        "orange"
-    ],
-    liquid: [
-        "pepsi",
-        "mazza",
-        "ice",
-        "water"
-    ],
-    toppings: [
-        "chocolate",
-        "peanuts"
-    ],
-    holder: [
-        "cone",
-        "cup",
-        "sticks"
-    ]
+// let stocks = {
+//     fruits: [
+//         "strawberry",
+//         "grapes",
+//         "Apple",
+//         "orange"
+//     ],
+//     liquid: [
+//         "pepsi",
+//         "mazza",
+//         "ice",
+//         "water"
+//     ],
+//     toppings: [
+//         "chocolate",
+//         "peanuts"
+//     ],
+//     holder: [
+//         "cone",
+//         "cup",
+//         "sticks"
+//     ]
+// }
+
+// let isShopOpenOrClose = false
+
+// // Made the order
+// let order = (time, work) => {
+
+//     return new Promise(
+//         (resolve, reject) => {
+//             // Shop open serve ice cream
+//             if (isShopOpenOrClose) {
+
+//                 setTimeout(() => {
+//                     resolve(
+//                         work()
+//                     )
+//                 }, time)
+
+//             }
+//             // Shop is close please try again later
+//             else {
+//                 reject(
+//                     console.log("Our Shop is Closed Please visit Again")
+//                 )
+//             }
+//         }
+//     )
+// }
+
+// // 1. Relation b/w time and work
+// order(2000, () => console.log(`${stocks.fruits[0]} was selected....[2s]`))
+
+//     .then(() => {
+//         return order(0, () => console.log('Production has Started....[0s]'))
+//     })
+
+//     .then(
+//         () => {
+//             return order(2000, () => console.log("the fruit was chopped....[2s]"))
+//         }
+//     )
+
+//     .then(
+//         () => {
+//             return order(1000, () => console.log(`${stocks.liquid[2]} & ${stocks.liquid[3]} was selected....[1s]`))
+//         }
+//     )
+
+//     .then(
+//         () => {
+//             return order(1000, () => console.log('start the Machine....[1s]'))
+//         }
+//     )
+//     .then(
+
+//         () => {
+//             return order(1000, () => console.log(`ice cream put on ${stocks.holder[0]}....[2s]`))
+//         }
+//     )
+
+//     .then(
+//         () => {
+//             return order(3000, () => console.log(`${stocks.toppings[0]} toppings are added....[2s]`))
+//         }
+//     )
+
+//     .then(
+//         () => {
+//             return order(1000, () => console.log("ready to serve....[3s]"))
+//         }
+//     )
+
+//     .catch(
+//         () => console.log("Customer Left the shop...")
+//     )
+
+//     // it always run
+//     .finally(
+//         () => {
+//             console.log("day ended, Shop is closed.....")
+//         })
+
+
+
+/**
+ * -----------------------------------------------------
+ *     Difference Between Async/Await and Promises
+ * -----------------------------------------------------
+*/
+
+// let stocks = {
+//     fruits: [
+//         "strawberry",
+//         "grapes",
+//         "Apple",
+//         "orange"
+//     ],
+//     liquid: [
+//         "pepsi",
+//         "mazza",
+//         "ice",
+//         "water"
+//     ],
+//     toppings: [
+//         "chocolate",
+//         "peanuts"
+//     ],
+//     holder: [
+//         "cone",
+//         "cup",
+//         "sticks"
+//     ]
+// }
+
+// let isShopOpenOrClose = true
+
+// function time(ms) {
+//     return new Promise(
+//         (resolve, reject) => {
+//             if (isShopOpenOrClose) {
+//                 setTimeout(resolve, ms)
+//             }
+//             else {
+//                 reject(
+//                     console.log("shop is close")
+//                 )
+//             }
+//         }
+//     )
+// }
+
+// async function kitchen() {
+//     try {
+//         await time(2000)
+//         console.log(`${stocks.fruits[0]}`)
+
+//         await time(0)
+//         console.log("start the Production")
+
+//         await time(2000)
+//         console.log("cut the fruit")
+
+//         await time(1000)
+//         console.log("Add water and ice")
+
+//         await time(1000)
+//         console.log("Start the machine")
+
+//         await time(2000)
+//         console.log("cone is selected")
+
+//         await time(3000)
+//         console.log("toppings are added")
+
+//         await time(2000)
+//         console.log("Ready to serve")
+
+//     }
+//     catch (error) {
+//         console.log("Customer left...")
+//     }
+//     finally {
+//         console.log("day ended shop close")
+//     }
+// }
+
+// kitchen()
+
+
+// let toppingChoice = () => {
+//     return new Promise(
+//         (resolve, reject) => {
+//             setTimeout(() => {
+//                 resolve(
+//                     console.log("Which topping would you love")
+//                 )
+//             }, 3000)
+//         }
+//     )
+// }
+
+// async function kitchen() {
+//     console.log(" a ")
+//     console.log(" b ")
+//     console.log(" c ")
+
+//     await toppingChoice()
+
+//     console.log(" d ")
+//     console.log(" e ")
+// }
+
+// kitchen()
+// console.log("doing the dishes")
+// console.log("cleaning the tables")
+// console.log("taking other order")
+
+
+// let order = () => {
+//     return new Promise(
+//         (reject, resolve) => {
+//             if (true) {
+
+//                 resolve(
+
+//                 )
+
+//             } else {
+
+//                 reject(
+
+//                 )
+
+//             }
+//         }
+//     )
+// }
+
+// try | catch | finally
+// async function order() {
+//     try {
+//         await abc;
+//     } catch (error) {
+//         console.log("abc does not exist..\n\n", error)
+//     } finally {
+//         console.log("\nrun code anyway")
+//     }
+// }
+
+// order()
+
+// .then(
+//     () =>{
+//         console.log('qwertyuiop')
+//     }
+// )
+
+
+// Channel - Akshay saini 
+/**
+ * -----------------------------------------------------
+ * 
+ *       1. what is Async ?
+ *       2. What is Await ?
+ *       3. how async work behind the scenes ?
+ *       4. Example using async / Await
+ *       5. Error handling
+ *       6. Interviews
+ *       7. Async await vs promise.then/.catch/.finally
+ * 
+ * -----------------------------------------------------
+*/
+
+// Note:
+// 1. this async function """"always return the promise""""
+// 2. there are two case | either you return the promise if you don't return a promise but return the non-promise value then it take the value wrap inside the promise and return that promise object.
+
+// async function getData() {
+//     // return "qwertyuiop"
+//     return new Promise(
+//         (resolve, reject) => {
+//             resolve("Promise Resolve value!!")
+//         }
+//     )
+// }
+
+// const dataPromise = getData()
+// console.log(dataPromise)   // Outcome: Promise { 'namaste' }
+
+// // How to fetch the value from Promise Object
+// dataPromise.then(res => console.log(res))
+
+
+
+
+
+// Async and await is use for handle Promises.
+// what is await: --> await can only be use inside the Async function
+
+/**
+ * -----------------------------------------------------------------------
+ *      Difference Between normal function and async..await function
+ * -----------------------------------------------------------------------
+ */
+
+
+
+let p = new Promise(
+    (resolve, reject) => {
+
+        setTimeout(
+            () => {
+                resolve('Promise Resolve.....!')
+            }, 5000)
+
+    }
+)
+
+let p2 = new Promise(
+    (resolve, reject) => {
+        setTimeout(() => {
+            resolve("this is the Second promise called........!")
+        }, 10000);
+    }
+)
+
+/**  -----> Normal Function <----- */
+
+// In Normal function js engine is not going to wait till the Promises Resolves, it simply execute line by line and when Promise is Resolved it automatically printed in the console.
+function getData() {
+    // js engine will not wait for promise resolve. and print the console
+    p.then(res => console.log(res))
+
+    console.log("namaste javascript........!")
 }
 
-let isShopOpenOrClose = true
 
-// Made the order
-let order = (time, work) => {
+/**  -----> Async Function with await inside <----- */
+// Code is waiting at line number 582 | It wait for the promise to resolve |  it go to next line ones the Promise is resolve.
+async function handlePromiseWithOnePromise() {
 
-    return new Promise(
-        (resolve, reject) => {
-            // Shop open serve ice cream
-            if (isShopOpenOrClose) {
+    console.log("Hello World, this is the above statement of Async await......!")
 
-                setTimeout(() => {
-                    resolve(
-                        work()
-                    )
-                }, time)
+    const val = await p
+    console.log("namaste js...........!")
+    console.log(val + "\n")
+}
+// handlePromiseWithOnePromise()  // it take 10 second to print the content present in function
 
-            }
-            // Shop is close please try again later
-            else {
-                reject(
-                    console.log("Our Shop is Closed Please visit Again")
-                )
-            }
-        }
-    )
+
+
+// it take the 10 second only print the content present inside the function in one go.
+//  if we change the Promise then also it Going to print the similar way
+async function handlePromise() {
+
+    console.log("Hello World, this is the above statement of Async await......!")
+
+    // after 5s it print this 
+    const val = await p
+    console.log("namaste js...........!")
+    console.log(val + "\n")
+
+    // Again print this after 5s total 10s it take 
+    const val2 = await p2
+    console.log("this is val-2 printing")
+    console.log(val2 + "\n")
 }
 
-// 1. Relation b/w time and work
-order(2000, () => console.log(`${stocks.fruits[0]} was selected....[2s]`))
+// How work Behind the scene:
+/**
+ * when it run it go Synchronous manner.
+
+* 1. CallStack empty initially: [<empty callStack>]
+
+* 2. function call => function come into the callStack: ["<handelPromise>""]
+
+* 3. now it start execution [<there are two promise have to Resolve at any particular time. [Async promise>]
+
+// * 5. in the execution await is called, at this moment the [<"handelPromise function">] is suspend and move out from the callBack. and it wait for p1 resolved and then only it move ahead.
+
+// * 7. after the time over the function is again call and ["<handelPromise>"] is come to the callStack. and again star execution where actually it left.
+
+// * 8. if there are again await called then again the same procedure take a place and move further [for 5 second again suspend the execution and wait until p2 completed]
+
+// * inshort await take a place function is suspended for Provided time period | and after that time period gets over it again call. if there are await called same process take a place.
+
+
+*/
+
+// getData()  // Outcome: Promise Resolve.....!
+handlePromise()
